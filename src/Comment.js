77200cce9784a15f4228b3a6bd15deba24a88007
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-27 11:19:31 +0200

    Add styles

--- src/Comment.js
+++ src/Comment.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import style from './Comment.css';
 
 const Comment = ({text, id, removeComment, editComment, likes, dislikes, thumbUpComment, thumbDownComment}) =>
-  <li>{text} <span>likes: {likes} dislikes: {dislikes}</span>
-    <button onClick={() => removeComment(id)}>Delete</button>
-    <button onClick={() => editComment(id, prompt('Edit comment'))}>Edit</button>
-    <button onClick={() => thumbUpComment(id)}>Thumb up</button>
-    <button onClick={() => thumbDownComment(id)}>Thumb down</button>
+  <li>
+    <h1>{text}</h1>
+    <span>likes: {likes}</span>
+    <span> dislikes: {dislikes}</span>
+    <button type="button" class="btn btn-success" onClick={() => thumbUpComment(id)}>Thumb up</button>
+    <button type="button" class="btn btn-warning" onClick={() => thumbDownComment(id)}>Thumb down</button>
+    <button type="button" class="btn btn-primary" onClick={() => editComment(id, prompt('Edit comment'))}>Edit</button>
+    <button type="button" class="btn btn-danger" onClick={() => removeComment(id)}>Delete</button>
   </li>;
 
 export default Comment;
